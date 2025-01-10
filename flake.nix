440393dceedb82b63b089accef8224b607f5431d
unwrinkled <geotsoulis@gmail.com>
2025-01-10 19:11:24 +0100

    chore: set nixpkgs to nixos-unstable branch

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "My NixOS Flake";
 
   inputs = {
-    # nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs.url = "nixpkgs/d70bd19e0a38ad4790d3913bf08fcbfc9eeca507";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
