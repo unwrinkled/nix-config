4b9330eb7a9254eb97930a8028d7182846f9cff9
unwrinkled <geotsoulis@gmail.com>
2025-07-05 08:41:40 +0200

    feat: switch timezone automatically

--- nixos/default.nix
+++ nixos/default.nix
@@ -29,7 +29,8 @@
   networking.networkmanager.enable = true;
 
   # Set your time zone.
-  time.timeZone = "Europe/Vienna";
+  time.timeZone = lib.mkDefault "Europe/Vienna";
+  services.automatic-timezoned.enable = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
